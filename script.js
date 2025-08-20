843f9d1db58b69918b41b14cebad59cd6262fa3e
Amine297 <chebbiamine.97@gmail.com>
2025-08-20 12:19:34 +0100

    pull

--- script.js
+++ script.js
@@ -19,3 +19,49 @@ function updateCart() {
 
   document.getElementById("total").textContent = total.toFixed(2);
 }
+// Sample featured products data
+const featuredProducts = [
+  {
+    id: 1,
+    name: "Crying Sticker",
+    price: 3.99,
+    image: "https://i.imgur.com/1bX5QH6.png",
+  },
+  {
+    id: 2,
+    name: "Sad Sticker",
+    price: 2.49,
+    image: "https://i.imgur.com/QphN04B.png",
+  },
+  {
+    id: 3,
+    name: "Funny Sticker",
+    price: 4.99,
+    image: "https://i.imgur.com/8Km9tLL.png",
+  },
+  {
+    id: 4,
+    name: "cool sticker",
+    price: 5.5,
+    image: "https://i.imgur.com/jtYpvt6.png",
+  },
+];
+
+const productsGrid = document.getElementById("featured-products");
+
+function renderProducts() {
+  productsGrid.innerHTML = "";
+  featuredProducts.forEach((product) => {
+    const card = document.createElement("div");
+    card.className = "product-card";
+    card.innerHTML = `
+      <img src="${product.image}" alt="${product.name}" />
+      <h3>${product.name}</h3>
+      <p class="price">$${product.price.toFixed(2)}</p>
+      <button>Add to Cart</button>
+    `;
+    productsGrid.appendChild(card);
+  });
+}
+
+renderProducts();
